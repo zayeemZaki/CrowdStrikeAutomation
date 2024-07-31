412eeaaa949e01fd5c38c2913dd9eb593ff456be
Rajiv Kaushik <zayeemzaki45@gmail.com>
2024-07-31 17:22:12 -0400

    added comments

--- AdminRights/RemoveAdminRights.py
+++ AdminRights/RemoveAdminRights.py
@@ -5,19 +5,20 @@ from GetRtrSessionId import initiateRtrSession
 
 #function to remove admin rights
 def removeAdminRights(token, session_id, username):
-    url = "https://api.crowdstrike.com/real-time-response/entities/execute-command/v1"
-    headers = {
-        "Authorization": f"Bearer {token}",
-        "Content-Type": "application/json"
-    }
-    data = {
-        "base_command": "runscript",
-        "command_string": f"Remove-LocalGroupMember -Group \"Administrators\" -Member \"{username}\"",
-        "session_id": session_id
-    }
-    response = requests.post(url, headers=headers, json=data)
-    response.raise_for_status()
-    return response.json()
+   print(token, session_id, username)
+   url = "https://api.crowdstrike.com/real-time-response/entities/active-responder-command/v1"
+   headers = {
+       "Authorization": f"Bearer {token}",
+       "Accept": "application/json"
+   }
+   data = {
+       "base_command": "runscript",
+       "command_string": f"Remove-LocalGroupMember -Group 'Administrators' -Member '{username}'",
+       "session_id": session_id
+   }
+   response = requests.post(url, headers=headers, json=data)
+   response.raise_for_status()
+   return response.json()
 
 #gets token
 token = getToken()
@@ -43,3 +44,8 @@ result = removeAdminRights(token, session_id, username)
 
 #prints final result
 print("Command Execution Result:", result)
+
+
+"""
+not working
+"""
--- StaleAccounts/getStaleAccounts.py
+++ StaleAccounts/getStaleAccounts.py
@@ -17,24 +17,75 @@ headers = {
     'Content-Type': 'application/json'
 }
 
-query = """
-query {
-  entities(types: [USER], stale: true, sortKey: RISK_SCORE, sortOrder: DESCENDING, first: 100) {
-    nodes {
-      primaryDisplayName
-      secondaryDisplayName
-      isHuman: hasRole(type: HumanUserAccountRole)
-      riskScore
-      riskScoreSeverity
-      accounts {
-        ... on ActiveDirectoryAccountDescriptor {
-          domain
+print("\nPlease enter how you want to sort the Stale Accounts:") 
+print(" 1. type \"risk score\" to sort based on risk score.")
+print(" 2. type \"is human\" to sort basd on if host is human.")
+print(" 3. type \"domain\" to sort based on the domain.")
+
+sortingBasedOn = input("-")
+
+if sortingBasedOn == "risk score":
+    query = """
+    query {
+      entities(types: [USER], stale: true, sortKey: RISK_SCORE, sortOrder: DESCENDING, first: 100) {
+        nodes {
+          primaryDisplayName
+          secondaryDisplayName
+          isHuman: hasRole(type: HumanUserAccountRole)
+          riskScore
+          riskScoreSeverity
+          accounts {
+            ... on ActiveDirectoryAccountDescriptor {
+              domain
+            }
+          }
+          {
+            countEntities(types: [USER] hasWeakPassword: true)
+          }
         }
       }
     }
-  }
-}
-"""
+    """
+elif sortingBasedOn == "is human":
+    query = """
+    query {
+      entities(types: [USER], stale: true, sortKey: IS_HUMAN, sortOrder: DESCENDING, first: 100) {
+        nodes {
+          primaryDisplayName
+          secondaryDisplayName
+          isHuman: hasRole(type: HumanUserAccountRole)
+          riskScore
+          riskScoreSeverity
+          accounts {
+            ... on ActiveDirectoryAccountDescriptor {
+              domain
+            }
+          }
+        }
+      }
+    }
+    """
+
+elif sortingBasedOn == "domain":
+    query = """
+    query {
+      entities(types: [USER], stale: true, sortKey: DOMAIN, sortOrder: DESCENDING, first: 100) {
+        nodes {
+          primaryDisplayName
+          secondaryDisplayName
+          isHuman: hasRole(type: HumanUserAccountRole)
+          riskScore
+          riskScoreSeverity
+          accounts {
+            ... on ActiveDirectoryAccountDescriptor {
+              domain
+            }
+          }
+        }
+      }
+    }
+    """    
+
 
 response = requests.post(graphqlUrl, headers=headers, json={'query': query})
 
@@ -49,7 +100,8 @@ if response.status_code == 200:
         isHuman = user.get('isHuman')
         riskScore = user.get('riskScore')
         riskScoreSeverity = user.get('riskScoreSeverity')
-        allStaleUsers.append((primaryName, secondaryName, isHuman, riskScore, riskScoreSeverity))
+        domain = user.get('domain')
+        allStaleUsers.append((primaryName, secondaryName, isHuman, riskScore, riskScoreSeverity, domain))
 
     data = {
         'Primary Name' : [user[0] for user in allStaleUsers],
@@ -57,6 +109,7 @@ if response.status_code == 200:
         'Is Human' : [user[2] for user in allStaleUsers],
         'Risk Score' : [user[3] for user in allStaleUsers],
         'Risk Severity' : [user[4] for user in allStaleUsers],
+        'Domain' : [user[5] for user in allStaleUsers],
     }
 
     pd.set_option('display.max_rows', None)
@@ -69,5 +122,10 @@ if response.status_code == 200:
 else:
     print('Failed to retrieve users:', response.status_code, response.text)
  
-#inactive_period
+"""
+inactive_period
 
+countEntities(types: [USER] stale: true)
+
+better if else
+"""
