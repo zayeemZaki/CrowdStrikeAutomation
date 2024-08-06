d74ef157a3dc08ae0217493c02411a27e620355e
Rajiv Kaushik <zayeemzaki45@gmail.com>
2024-08-06 10:21:33 -0400

    added falconpy

--- AdminRights/RemoveAdminRights.py
+++ AdminRights/RemoveAdminRights.py
@@ -2,6 +2,14 @@ import requests
 from GetToken import getToken   
 from GetDeviceId import getDeviceId
 from GetRtrSessionId import initiateRtrSession
+from LoadConfig import load_config # loads config.yaml
+
+from falconpy import RealTimeResponse
+
+
+config = load_config('config.yaml')
+
+
 
 # Function to remove admin rights
 def removeAdminRights(token, session_id, username):
@@ -15,39 +23,49 @@ def removeAdminRights(token, session_id, username):
     data = {
         "base_command": "runscript",
         "command_string": f"Remove-LocalGroupMember -Group 'Administrators' -Member '{username}'",
+        "persist": True,
         "session_id": session_id
     }
     response = requests.post(url, headers=headers, json=data)
     response.raise_for_status()
     return response.json()
 
-# Main execution flow
-if __name__ == "__main__":
-    # Gets token
-    token = getToken()
-    if token: 
-        print('Authentication successful')
-    else:
-        print('Authentication failed:')
-        exit()
+# Gets token
+token = getToken()
+if token: 
+    print('Authentication successful')
+else:
+    print('Authentication failed:')
+    exit()
 
-    # Gets device ID from hostname
-    hostname = input("Please input target hostname: ") # Takes hostname as input
-    device_id = getDeviceId(token, hostname)
+# Gets device ID from hostname
+hostname = input("Please input target hostname: ") # Takes hostname as input
+device_id = getDeviceId(token, hostname)
 
-    # Gets session ID using device ID
-    session_id = initiateRtrSession(token, device_id)
+# Gets session ID using device ID
+session_id = initiateRtrSession(token, device_id)
 
-    # Takes username as input
-    username = input("Please enter target device username: ")
+# Takes username as input
+username = input("Please enter target device username: ")
 
-    # Calls remove admin rights function and stores the result in result variable
-    result = removeAdminRights(token, session_id, username)
-
-    # Prints final result
-    print("Command Execution Result:", result)
+# Calls remove admin rights function and stores the result in result variable
+# result = removeAdminRights(token, session_id, username)
 
 
+# Do not hardcode API credentials!
+falcon = RealTimeResponse(client_id=config['client_id'],
+                          client_secret=config['client_id']
+                          )
+
+response = falcon.execute_active_responder_command(base_command = "runscript",
+                                                   command_string = f"Remove-LocalGroupMember -Group 'Administrators' -Member '{username}'",
+                                                   persist = True,
+                                                   session_id = session_id
+                                                   )
+print(response)
+
+# Prints final result
+# print("Command Execution Result:", result)
 
 
 
