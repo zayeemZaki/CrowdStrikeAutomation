a37e8db9d7a452493aab14f60f4fbd4983280f41
Rajiv Kaushik <zayeemzaki45@gmail.com>
2024-10-17 10:43:32 -0400

    updated startScan

--- WindowsScan/startScan.py
+++ WindowsScan/startScan.py
@@ -14,7 +14,7 @@ def start_scan(token, hosts, host_groups, file_paths):
         'file_paths': file_paths,
         'scan_exclusions': [],
         'initiated_from': 'falcon_adhoc',
-        'cpu_priority': 1, 
+        'cpu_priority': 1,
         'description': 'API Scan Initiated',
         'quarantine': True,
         'endpoint_notification': True,
@@ -37,6 +37,7 @@ def start_scan(token, hosts, host_groups, file_paths):
         print('Failed to start scan.')
         print(f'Status code: {response.status_code}')
         print(f'Response: {response.text}')
+        return None
 
 def get_scan_details(token, scan_id):
     url = f'https://api.crowdstrike.com/ods/entities/scans/v1?ids={scan_id}'
@@ -53,7 +54,6 @@ def get_scan_details(token, scan_id):
         print('Failed to retrieve scan details.')
         print(f'Status code: {response.status_code}')
         print(f'Response: {response.text}')
-
 
 def find_malicious_file_ids(token, filter_query='', offset=0, limit=5, sort=''):
     url = f'https://api.crowdstrike.com/ods/queries/malicious-files/v1?filter={filter_query}&offset={offset}&limit={limit}&sort={sort}'
@@ -76,12 +76,13 @@ def find_malicious_file_ids(token, filter_query='', offset=0, limit=5, sort=''):
 
 def get_malicious_file_details(token, file_ids):
     base_url = 'https://api.crowdstrike.com/ods/entities/malicious-files/v1'
-    params = {
-        'ids': file_ids
-    }
     headers = {
         'Content-Type': 'application/json',
         'Authorization': f'Bearer {token}'
+    }
+
+    params = {
+        'ids': ','.join(file_ids)
     }
     
     response = requests.get(base_url, headers=headers, params=params)
@@ -94,34 +95,31 @@ def get_malicious_file_details(token, file_ids):
         print(f'Status code: {response.status_code}')
         print(f'Response: {response.text}')
 
-
-
-
-
+# Obtain the token
 token = getToken()
+
+# Define scan parameters
 hosts = []
 host_groups = []  
 file_paths = ['C:\\Windows']
 
-start_scan(token, hosts, host_groups, file_paths) #start scan
-scan_id = start_scan(token, hosts[0], file_paths)
+# Start the scan
+scan_id = start_scan(token, hosts, host_groups, file_paths)
 if scan_id:
-    get_scan_details(token, scan_id) #get scan details
+    # Get scan details
+    get_scan_details(token, scan_id)
 
-
-# find malicious file ids!
-
+# Find malicious file IDs
 filter_query = ''
 offset = 0
 limit = 5
 sort = ''
 
 malicious_file_ids = find_malicious_file_ids(token, filter_query, offset, limit, sort)
-
 if malicious_file_ids:
     print('List of Malicious File IDs:')
     for file_id in malicious_file_ids:
         print(file_id)
 
-# get malicious file ids details!
-get_malicious_file_details(token, malicious_file_ids)
+    # Get details for malicious file IDs
+    get_malicious_file_details(token, malicious_file_ids)
