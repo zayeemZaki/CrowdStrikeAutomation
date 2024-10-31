d7a04ad5f704de69a5131e7bcb5d4f48fc62a5ff
Rajiv Kaushik <zayeemzaki45@gmail.com>
2024-10-31 13:54:46 -0400

    updated odsScan.py

--- WindowsScan/odsScan.py
+++ WindowsScan/odsScan.py
@@ -1,7 +1,7 @@
 import os
 import sys
 import yaml
-from falconpy import ODS
+from falconpy import APIHarnessV2
 
 def load_config(file_path):
     """Load configuration from a YAML file."""
@@ -19,27 +19,30 @@ def load_config(file_path):
 def main():
     config = load_config('config.yaml')
 
-    falcon = ODS(client_id=config['client_id'], client_secret=config['client_secret'])
+    falcon = APIHarnessV2(client_id=config['client_id'], 
+                 client_secret=config['client_secret']
+                 )
 
-    scan_payload = {
-        "cloud_ml_level_detection": 0,
-        "cloud_ml_level_prevention": 0,
-        "cpu_priority": 1,
+    BODY = {
+        "cloud_ml_level_detection": 2,
+        "cloud_ml_level_prevention": 2,
+        "cpu_priority": 2,
         "description": "On Demand Scan Description",
         "endpoint_notification": True,
-        "file_paths": ["C:\\\\"],
+        "file_paths": ["C:\\Windows"],
         "hosts": ["host_id"],
         "initiated_from": "manual",
-        "max_duration": 0,
-        "max_file_size": 0,
-        "pause_duration": 0,
+        "max_duration": 2,
+        "max_file_size": 60,
+        "pause_duration": 2,
         "quarantine": True,
-        "sensor_ml_level_detection": 0,
-        "sensor_ml_level_prevention": 0
+        "sensor_ml_level_detection": 2,
+        "sensor_ml_level_prevention": 2
     }
 
     try:
-        response = falcon.create_scan(body=scan_payload)
+        response = falcon.command("create_scan", body=BODY)
+
         
         if response.get('status_code') == 200 or response.get('status_code') == 201:
             print("Scan created successfully.")
